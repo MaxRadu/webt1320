ae6b339f1ba6c33f9f20a50eabcfbbd8c4538689
MaxRadu <maximillianradu@gmail.com>
2021-07-27 18:31:43 -0400

    added jQuery

---
+++ omnifood/resources/javascript/script.js
@@ -0,0 +1,35 @@
+// Scroll on buttons //
+$(document).ready(function() {
+
+    $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname)
+    {var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1300);
+      return false;
+            }
+        }
+    });
+});
+
+$('.js--scroll-to-store-page').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-store-page').offset().top}, 1300);
+});
+
+$('.js--scroll-to-about-page').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-about-page').offset().top}, 1300);
+});
+
+$('.js--scroll-to-contact-page').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-contact-page').offset().top}, 1300);
+});
+
+});
+
+
+
+
