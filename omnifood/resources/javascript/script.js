9550e1537b62728da938cda21951ace1ddefeb91
MaxRadu <maximillianradu@gmail.com>
2021-07-28 16:38:42 -0400

    updated smooth scroll

--- omnifood/resources/javascript/script.js
+++ omnifood/resources/javascript/script.js
@@ -1,40 +1,28 @@
 // Scroll on buttons //
 $(document).ready(function() {
 
-    $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname)
-    {var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1300);
-      return false;
-            }
-        }
-    });
-});
-
 $('.js--scroll-to-store-page').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-store-page').offset().top}, 1300);
+    $('html, body').animate({scrollTop: $('.js--section-store-page').offset().top}, 1000);
 });
 
 $('.js--scroll-to-about-page').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-about-page').offset().top}, 1300);
+    $('html, body').animate({scrollTop: $('.js--section-about-page').offset().top}, 1000);
 });
 
 $('.js--scroll-to-contact-page').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-contact-page').offset().top}, 1300);
+    $('html, body').animate({scrollTop: $('.js--section-contact-page').offset().top}, 1000);
 });
 
 
 // Animations on scroll
-$('.js--wp-1').waypoint(function() {
-    $('.js--wp-1').addClass('animated fadeIn');
-})
+$('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeInUp');
+}, {
+    offset: '50%'
 
 
+
+});
 
 });
 
